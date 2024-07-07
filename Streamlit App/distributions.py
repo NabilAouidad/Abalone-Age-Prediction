c180b1d00920f9aa99be09f110b653f9d37406b0
Nabil Aouidad <nabil.aouidad@outlook.com>
2024-07-07 18:45:23 +0100

    Changed some parameters to meet the dataset's requirements

--- Streamlit App/distributions.py
+++ Streamlit App/distributions.py
@@ -6,13 +6,13 @@ from plotly.subplots import make_subplots
 def plotBars(df):
     num_cols = list(df.describe(include = np.number).columns)
     obj_cols = list(df.describe(include = np.object_).columns)
-    n_rows, n_cols = 3, 3
+    n_rows, n_cols = 1, 1
     c = 0
     fig = make_subplots(rows = n_rows, cols = n_cols,
-                        column_widths = [1500, 1500, 1500], 
-                        row_heights = [1500, 1500, 1500],
+                        column_widths = [500], 
+                        row_heights = [500],
                         horizontal_spacing = 0.13, vertical_spacing = 0.13)
-    fig.update_layout({"height":1000, "width":1500})
+    fig.update_layout({"height":500, "width":750})
 
     for i in range(1, n_rows+1):
         for j in range(1, n_cols+1):
@@ -30,18 +30,18 @@ def plotBars(df):
 def plotHistograms(df):
     num_cols = list(df.describe(include = np.number).columns)
     obj_cols = list(df.describe(include = np.object_).columns)
-    n_rows, n_cols = 3, 2
-    colors = ["red", "magenta", "green", "yellow", "orange", "blue"]
+    n_rows, n_cols = 3, 3
+    colors = ["red", "magenta", "green", "yellow", "orange", "blue", "red", "magenta", "green"]
     c = 0
     fig = make_subplots(rows = n_rows, cols = n_cols,
-                        column_widths = [1000, 1000], 
+                        column_widths = [1000, 1000, 1000], 
                         row_heights = [1000, 1000, 1000],
                         horizontal_spacing = 0.13, vertical_spacing = 0.17)
     fig.update_layout({"height":900, "width":900})
     
     for i in range(1, n_rows+1):
         for j in range(1, n_cols+1):
-            if i*j > len(obj_cols):
+            if i*j > len(num_cols):
                 break
             else :
                 fig.add_trace(go.Histogram(x = df[num_cols[c]],
