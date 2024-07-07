93f5519daea096b288bc26f1e7ce1399200b9603
Nabil Aouidad <nabil.aouidad@outlook.com>
2024-07-07 18:49:18 +0100

    Data correlations and distributions

--- Streamlit App/App.py
+++ Streamlit App/App.py
@@ -28,15 +28,48 @@ container.write('<h1 style="text-align: center;">Exploratory Data Analysis of th
 
 st.empty()
 
+#-----------------------------------------------------------
+
+abalone_df = pd.read_csv("abalone.csv")
+
+def showDatasetInfo():
+    shapeColumn, featuresColumn, descriptiveStatsColumn = st.columns(3)
+
+    shapeColumn.subheader("Shape")
+    shapeColumn.write(abalone_df.shape)
+
+    featuresColumn.subheader("Features")
+    featuresColumn.write(pd.DataFrame({"Column Name" : abalone_df.columns}))
+
+    descriptiveStatsColumn.subheader("Descriptive Statistics")
+    descriptiveStatsColumn.dataframe(abalone_df.describe())
+
+    st.subheader("Data Sample")
+    st.dataframe(abalone_df.sample(20))
+
+def showDataDistributions():
+    st.subheader("Bar Plots")
+    st.plotly_chart(plotBars(abalone_df))
+
+    st.subheader("Histograms")
+    st.plotly_chart(plotHistograms(abalone_df))
+
+def showCorrelations():
+    st.subheader("Pair Plot")
+    st.plotly_chart(plotPairPlots(abalone_df))
+
+    st.subheader("Heat Map")
+    st.plotly_chart(plotHeatMap(abalone_df))
+
 st.sidebar.title("EDA Options")
 
 box_values = st.sidebar.selectbox(" ", options = ["About the dataset", "Data Distributions", "Correlations"])
 
 if box_values == "About the dataset":
-    st.write("Hello World")
+    showDatasetInfo()
 
 if box_values == "Data Distributions":
-    st.write("Hello Algeria")
+    showDataDistributions()
 
 if box_values == "Correlations":
-    st.write("Hello Beni Kouffi")
+    showCorrelations()
