6f53504157c43e2039569454cfcc17bdc7edb2bc
Nabil Aouidad <nabil.aouidad@outlook.com>
2024-07-08 17:34:52 +0100

    Added the color scales

--- Streamlit App/correlations.py
+++ Streamlit App/correlations.py
@@ -5,7 +5,8 @@ import plotly.graph_objects as go
 def plotPairPlots(df):
     num_cols = list(df.describe(include = np.number).columns)
     obj_cols = list(df.describe(include = np.object_).columns)
-    fig = go.Figure(data = px.scatter_matrix(df[num_cols], 
+    fig = go.Figure(data = px.scatter_matrix(df[num_cols],
+                                             color = df["Sex"],
                                          dimensions = num_cols,
                                            width = 1000, height = 1000)
     )
@@ -16,6 +17,7 @@ def plotHeatMap(df):
     obj_cols = list(df.describe(include = np.object_).columns)
     correlation_matrix = df[num_cols].corr()
     fig = go.Figure(data = px.imshow(correlation_matrix,
+                                     color_continuous_scale = "Inferno",
                                      text_auto = ".3f",
                                      width = 1000, height = 1000)
     )
