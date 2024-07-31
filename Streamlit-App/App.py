c6d84e8370c97c0024966459deb08592ee48b83c
Nabil Aouidad <nabil.aouidad@outlook.com>
2024-07-31 13:32:18 +0100

    Updated streamlit

--- Streamlit-App/App.py
+++ Streamlit-App/App.py
@@ -78,8 +78,8 @@ if box_values == "EDA":
 model = joblib.load("AbalonePredictor.pkl")
 
 if box_values == "Make Predictions":
-    length = st.number_input("Length")
-    diam = st.number_input("Diameter")
+    length = st.number_input("Length", 0.0)
+    diam = st.number_input("Diameter", 0.0)
     height = st.number_input("Height", 0.0, 1.15)
     whole = st.number_input("Whole Weight", 0.0, 3.0)
     shucked = st.number_input("Shucked Weight", 0.0, 1.5)
@@ -87,22 +87,17 @@ if box_values == "Make Predictions":
     shell = st.number_input("Shell Weight", 0.0, 1.0)
     sex_selectbox = st.selectbox("Sample Sex", options = ["M", "F", "I"])
 
-    try:
+    if sex_selectbox == "M":
+        st.subheader("Sample Age")
+        X = model.predict([[length, diam, height, whole, shucked, viscera, shell, 1.0, 0.0, 0.0]]) + 1.5
+        st.write(X[0])
 
-        if sex_selectbox == "M":
-            st.subheader("Sample Age")
-            X = model.predict([[length, diam, height, whole, shucked, viscera, shell, 1.0, 0.0, 0.0]]) + 1.5
-            st.write(X[0])
+    if sex_selectbox == "F":
+        st.subheader("Sample Age")
+        X = model.predict([[length, diam, height, whole, shucked, viscera, shell, 0.0, 1.0, 0.0]]) + 1.5
+        st.write(X[0])
 
-        if sex_selectbox == "F":
-            st.subheader("Sample Age")
-            X = model.predict([[length, diam, height, whole, shucked, viscera, shell, 0.0, 1.0, 0.0]]) + 1.5
-            st.write(X[0])
-
-        if sex_selectbox == "I":
-            st.subheader("Sample Age")
-            X = model.predict([[length, diam, height, whole, shucked, viscera, shell, 0.0, 0.0, 1.0]]) + 1.5
-            st.write(X[0])
-    except Exception as e:
-        st.write(f"Error during prediction: {e}")
-        logging.error("Error during prediction", exc_info=True)
+    if sex_selectbox == "I":
+        st.subheader("Sample Age")
+        X = model.predict([[length, diam, height, whole, shucked, viscera, shell, 0.0, 0.0, 1.0]]) + 1.5
+        st.write(X[0])
