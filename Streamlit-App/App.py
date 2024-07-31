dbcda9277ccc07b8b9d179e1dc1a815320f3c950
Nabil Aouidad <nabil.aouidad@outlook.com>
2024-07-31 14:09:38 +0100

    Used another model

--- Streamlit-App/App.py
+++ Streamlit-App/App.py
@@ -72,7 +72,7 @@ box_values = st.sidebar.selectbox(" ", options = ["EDA", "Make Predictions"])
 if box_values == "EDA":
     eda()
 
-model = joblib.load("AbalonePredictor.pkl")
+model = joblib.load("eNetPredictor.pkl")
 
 if box_values == "Make Predictions":
     length = st.number_input("Length", 0.0)
